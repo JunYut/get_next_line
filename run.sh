fd06f1c67d4c2a6ffbe708b886c70d35140a6362
Tan Jun Yu <tanjunyu8888@gmail.com>
2023-11-10 16:36:10 +0800

    run.sh: added option to compile with buffer size specified

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 COMPILE=$1
 MEMCHECK=$2
+BUFFER=$3
 
 if [ "$COMPILE" -eq 1 ]; then
-	cc -g -Wall -Wextra -Werror *.c -o executable
+	cc -g -Wall -Wextra -Werror -D BUFFER_SIZE=$BUFFER *.c -o executable
 elif [ "$COMPILE" -eq -1]; then
 	rm -f executable
+else
+	cc -g -Wall -Wextra -Werror *.c -o executable
 fi
 
 if [ "$MEMCHECK" = "M" ]; then
