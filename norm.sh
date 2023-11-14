6723091b8ec9d908e1cb59cc7fb49ac0ad012170
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-14 16:00:11 +0800

    norm.sh: created script with option to check norm with or without bonus files

---
+++ norm.sh
@@ -0,0 +1,9 @@
+BONUS=$1
+
+if [ "$BONUS" = "b" ]; then
+	norminette -R CheckDefine *.h | grep "_bonus"
+	norminette *.c | grep "_bonus"
+else
+	norminette -R CheckDefine *.h | grep -v "_bonus"
+	norminette *.c | grep -v "_bonus"
+fi
