cf4b4d2e1b6ea45e160393b2e3f7cf1330d40627
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-14 16:03:31 +0800

    get_next_line.h: (void)

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include "get_next_line.h"
 
+#include <fcntl.h>
 #include <stdio.h>
 
 //_____________________________________________________________________________
 int main(void)
 {
+	// Read from text file
+	printf("Read from text file:\n");
+	int fd = open("text.txt", O_RDONLY);
+	char* str = get_next_line(fd);
+	printf("line 1: %s\n", str);
+	
+	str = get_next_line(fd);
+	printf("line 2: %s\n", str);
 
+	str = get_next_line(fd);
+	printf("line 3: %s\n", str);
+
+	str = get_next_line(fd);
+	printf("line 4: %s\n", str);
+
+	close(fd);
 }
