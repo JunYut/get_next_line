cd4f08bffebb47140cd50ce2b86c00e616d3d8b4
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-14 14:30:02 +0800

    run_zsh.sh: (void)

--- run_zsh.sh
+++ run_zsh.sh
@@ -4,16 +4,16 @@ BUFFER=$3
 
 if [ "$COMPILE" -eq 1 ]; then
 	cc -g -Wall -Wextra -Werror -D BUFFER_SIZE=$BUFFER *.c -o executable
-elif [ "$COMPILE" -eq -1]; then
-	rm -f executable
-else
+elif [ "$COMPILE" -eq 0 ]; then
 	cc -g -Wall -Wextra -Werror *.c -o executable
+elif [ "$COMPILE" -eq -1 ]; then
+	rm -f executable
 fi
 
 if [ "$MEMCHECK" = "M" ]; then
 	valgrind --tool=memcheck ./executable
 elif [ "$MEMCHECK" = "v" ]; then
 	valgrind ./executable
-else
+elif [ "$MEMCHECK" = 0 ]; then
 	./executable
 fi
