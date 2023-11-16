368c079983cf55601dcd6e5d59ad37caa094b1c4
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-16 15:48:10 +0800

    get_next_line.h: - added header guard - included header files - defined BUFFER_SIZE - declared function prototype of 'get_next_line()'
    get_next_line.h: added declaration of linked list

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
+/*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:30:22 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/16 15:30:23 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/16 21:12:54 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+typedef struct s_list
+{
+	char			*line;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+
+#endif
